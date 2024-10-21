5f18d3b23199b6660d7cc6e3ca87cc8cde990a1b
akshayraj-1 <akshayraj4101@gmail.com>
2024-10-21 16:04:33 +0530

    Added url.controller.js

---
+++ controllers/url.controller.js
@@ -0,0 +1,38 @@
+const  UrlModel = require("../models/url.model");
+const { isAuthenticated } = require('../middlewares/auth.middleware');
+
+exports.createShortenUrl = async (req, res) => {
+    try {
+        const { url } = req.body;
+        if (!url || !new RegExp(/(^https:\/\/.*)/g).test(url)) {
+            return res.status(400).json({ success: false, message: "Url is required" });
+        }
+        const userId = isAuthenticated(req) ? req.session.user._id : null;
+        const urlDoc = new UrlModel({
+            originalUrl: url,
+            shortenUrl: Math.random().toString(36).slice(2, 10),
+            creator: userId
+        });
+        await urlDoc.save();
+        return res.json({ success: true, message: "Url created successfully", data: { originalUrl: url, shortenUrl: req.headers.host + "/" + urlDoc.shortenUrl } });
+    } catch (error) {
+        return res.status(500).json({ success: false, message: error.message });
+    }
+}
+
+exports.getOriginalUrl = async (req, res) => {
+   try {
+       const { shortUrlId } = req.params;
+       const urlDoc = await UrlModel.findOneAndUpdate(
+           { shortenUrl: shortUrlId, status: "active" },
+           { $inc: { clicks: 1 } }
+       );
+       if (urlDoc && urlDoc.originalUrl) {
+           return res.redirect(urlDoc.originalUrl);
+       } else {
+           throw new Error("Url not found");
+       }
+   } catch (error) {
+       res.render("404", { error: error.message });
+   }
+}
