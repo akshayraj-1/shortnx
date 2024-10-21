9e23fb0bdee5f759d096024a6d6c31768f6bd58c
akshayraj-1 <akshayraj4101@gmail.com>
2024-10-21 16:05:14 +0530

    Modified tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,14 +7,15 @@ module.exports = {
         primary: '#6139ff',
         background: '#f8f9fb',
         surface: '#ffffff',
-        surfaceVariant: '#f7f8fa',
+        surfaceVariant: '#f7f9fc',
         textPrimary: '#2f2e41',
         textSecondary: '#878899',
-        textTertiary: '#bdc4cd',
+        textTertiary: '#d8dde6',
       },
       backgroundImage: {
         'emphasis-short': 'url("/images/emphasizing-short.svg")',
         'emphasis-wide': 'url("/images/emphasizing-wide-.svg")',
+        noise: 'url("/images/noise.jpg")',
         star: 'url("/images/star.svg")',
       },
       fontFamily: {
