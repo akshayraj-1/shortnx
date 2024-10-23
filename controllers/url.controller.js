426b42db06e60a7843a38e0e13937a072920380c
akshayraj-1 <akshayraj4101@gmail.com>
2024-10-24 01:34:52 +0530

    Modified url.controller.js

--- controllers/url.controller.js
+++ controllers/url.controller.js
@@ -1,5 +1,10 @@
-const  UrlModel = require("../models/url.model");
+const UAParser = require("ua-parser-js");
 const { isAuthenticated } = require('../middlewares/auth.middleware');
+
+const UrlModel = require("../models/url.model");
+const UrlAnalyticsModel = require("../models/urlanalytics.model");
+
+const parser = new UAParser();
 
 exports.createShortenUrl = async (req, res) => {
     try {
@@ -28,11 +33,36 @@ exports.getOriginalUrl = async (req, res) => {
            { $inc: { clicks: 1 } }
        );
        if (urlDoc && urlDoc.originalUrl) {
+           await updateUrlAnalytics(req, shortUrlId, urlDoc.originalUrl)
            return res.redirect(urlDoc.originalUrl);
        } else {
            throw new Error("Url not found");
        }
    } catch (error) {
+       console.error(error);
        res.render("pages/404", { title: "Page Not Found", error: error.message });
    }
+}
+
+
+async function updateUrlAnalytics(req, shortUrlId, originalUrl) {
+    const localIp = req.ip;
+    const response  = await fetch("https://ipinfo.io/" + localIp + "/json");
+    const data = await response.json();
+    const geoLocation = {
+        city: data?.city,
+        country: data?.country,
+        region: data?.region
+    }
+    const uaResults = parser.setUA(req.headers["user-agent"]).getResult();
+    const urlAnalytics = new UrlAnalyticsModel({
+        shortenUrl: shortUrlId,
+        originalUrl: originalUrl,
+        localIp,
+        geoLocation,
+        userAgent: req.headers["user-agent"],
+        deviceType: uaResults.device.type,
+        platform: uaResults.os.name
+    });
+    return await urlAnalytics.save();
 }
