95f051825c05b69798bf805ba509f3d055474a39
akshayraj-1 <akshayraj4101@gmail.com>
2024-10-23 23:08:54 +0530

    Deleted .gitignore

--- controllers/user.controller.js
+++
@@ -1,9 +0,0 @@
-const { checkUserAuth } = require("../middlewares/auth.middleware");
-
-exports.getDashboard = [checkUserAuth, (req, res, next) => {
-    return res.render("pages/user-dashboard", { title: res.locals.user.name });
-}];
-
-exports.getShortenUrls = [checkUserAuth, (req, res, next) => {
-    // TODO:
-}];
