681d3a0e06af7200a09a05b908af0bbfe04f211f
akshayraj-1 <akshayraj4101@gmail.com>
2024-11-15 16:01:07 +0530

    Modified url.controller.js

--- src/controllers/url.controller.js
+++ src/controllers/url.controller.js
@@ -1,45 +1,50 @@
 const UrlModel = require("../models/url.model");
 const UrlAnalyticsModel = require("../models/urlanalytics.model");
-const { isAuthenticated } = require('../middlewares/auth.middleware');
+const { isAuthenticated, checkUserAuth } = require('../middlewares/auth.middleware');
 const { disableCache } = require("../middlewares/cache.middleware");
 const clientRequestInfo = require("../utils/requestInfo.util");
 
 
 exports.createShortenUrl = async (req, res) => {
+    const { url } = req.body;
+    if (!url || !new RegExp(/(^https:\/\/.*)|(^http:\/\/.*)/g).test(url)) {
+        res.status(400).json({success: false, message: "Please enter a valid url"});
+        return;
+    }
+    const userId = isAuthenticated(req) ? req.session.user._id : null;
+
     try {
-        const {url} = req.body;
-        if (!url || !new RegExp(/(^https:\/\/.*)|(^http:\/\/.*)/g).test(url)) {
-            return res.status(400).json({success: false, message: "Please enter a valid url"});
-        }
-        const userId = isAuthenticated(req) ? req.session.user._id : null;
         const urlDoc = new UrlModel({
             originalUrl: url,
             shortenUrl: Math.random().toString(36).slice(2, 10),
             creator: userId
         });
         await urlDoc.save();
-        // await new Promise(r => setTimeout(r, 4000)); // Testing Purpose
-        return res.status(201).json({
+        res.status(201).json({
             success: true,
             message: "Url created successfully",
-            data: {originalUrl: url, shortenUrl: req.headers.host + "/" + urlDoc.shortenUrl}
+            data: { originalUrl: url, shortenUrl: req.headers.host + "/" + urlDoc.shortenUrl }
         });
     } catch (error) {
-        return res.status(500).json({success: false, message: error.message});
+        res.status(500).json({success: false, message: error.message});
     }
 }
 
+exports.getShortenUrls = [checkUserAuth, (req, res, next) => {
+    // TODO: Implement get shorten urls of user
+}];
+
 exports.getOriginalUrl = [disableCache, async (req, res) => {
+    const { shortUrlId } = req.params;
+
     try {
-        const { shortUrlId } = req.params;
         const urlDoc = await UrlModel.findOneAndUpdate(
             { shortenUrl: shortUrlId, status: "active" },
-            { $inc: { clicks: 1 } },
+            { $inc: { clicks: 1 } }
         );
         if (urlDoc && urlDoc.originalUrl) {
             await updateUrlAnalytics(req, shortUrlId, urlDoc.originalUrl);
             res.status(302).redirect(urlDoc.originalUrl);
-            res.end();
         } else {
             res.render("pages/404", {title: "Page Not Found", error: "Url not found"});
         }
