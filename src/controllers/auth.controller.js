6a4d0bb16f4c353e2ea6ec06215a0daa48f84b88
akshayraj-1 <akshayraj4101@gmail.com>
2024-11-15 16:01:04 +0530

    Modified auth.controller.js

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,33 +1,39 @@
 const UserModel = require("../models/user.model");
-const  { isAuthenticated, checkUserAuth } = require('../middlewares/auth.middleware');
-
+const { isAuthenticated, checkUserAuth } = require('../middlewares/auth.middleware');
 
 // Get Login Page
 exports.getLogin = (req, res) => {
-    if (isAuthenticated(req)) {
-        return res.redirect("/user-dashboard");
-    } else {
-        return res.render("pages/login", { title: "Login | URL Shortener"});
-    }
+    isAuthenticated(req)
+        ? res.redirect("/user-dashboard")
+        : res.render("pages/login", { title: "Login | URL Shortener" });
 }
 
 // Get Signup Page
 exports.getSignup = (req, res) => {
-    if (isAuthenticated(req)) {
-        return res.redirect("/user-dashboard");
-    } else {
-        return res.render("pages/signup", { title: "Sign Up | URL Shortener" });
-    }
+    isAuthenticated(req)
+        ? res.redirect("/user-dashboard")
+        : res.render("pages/signup", { title: "Sign Up | URL Shortener" });
 }
 
-exports.loginUser = async (req, res) => {
+exports.login = async (req, res) => {
     const { email, password } = req.body;
     // TODO: Implement login
 }
 
-exports.signupUser = (req, res) => {
+exports.signUp = async (req, res) => {
     const { name, email, password } = req.body;
-    // TODO: Implement signup
+    if (!name || !email || !password) {
+        res.json({ success: false, message: "All fields are required" });
+        return;
+    }
+
+    try {
+        // TODO: Implement the signup
+        return res.json({success: true, message: "User created successfully"});
+    } catch (error) {
+        console.log(error.message);
+        return res.json({ success: false, message: error.code });
+    }
 }
 
 exports.googleAuth = (req, res) => {
