7e7ad3a0e4feaf4d29ac05a496c277ece02d369d
akshayraj-1 <akshayraj4101@gmail.com>
2024-11-15 16:01:07 +0530

    Modified user.controller.js

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,6 @@
 const { checkUserAuth } = require("../middlewares/auth.middleware");
 
 exports.getDashboard = [checkUserAuth, (req, res, next) => {
-    return res.render("pages/user-dashboard", { title: res.locals.user.name });
+    res.render("pages/dashboard", { title: res.locals.user.name });
 }];
 
-exports.getShortenUrls = [checkUserAuth, (req, res, next) => {
-    // TODO:
-}];
