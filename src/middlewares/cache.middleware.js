b4b9952e1cefbaab4aa33ebce9b93022d8472adf
akshayraj-1 <akshayraj4101@gmail.com>
2024-11-17 00:02:54 +0530

    Modified cache.middleware.js

--- src/middlewares/cache.middleware.js
+++ src/middlewares/cache.middleware.js
@@ -1,12 +1,14 @@
-exports.enableCache = (req, res, next) => {
+function enableCache(req, res, next) {
     res.set("Cache-Control", "public, max-age=604800"); // 7 days
     res.set("Expires", new Date(Date.now() + 604800000).toUTCString());
     next();
 }
 
-exports.disableCache = (req, res, next) => {
+function disableCache(req, res, next) {
     res.set("Cache-Control", "no-cache, no-store, must-revalidate");
     res.set("Pragma", "no-cache");
     res.set("Expires", "0");
     next();
 }
+
+module.exports = { enableCache, disableCache };
