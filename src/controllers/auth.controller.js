81d29fca56014b85565dd3874005b2c47c0779e6
akshayraj-1 <akshayraj4101@gmail.com>
2024-11-18 01:18:34 +0530

    Modified auth.controller.js

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,18 +1,19 @@
 const UserModel = require("../models/user.model");
 const { isAuthenticated, checkUserAuth } = require('../middlewares/auth.middleware');
 const { signInWithEmail, signUpWithEmail } = require("../services/firebase-services");
+const getAuthErrorMessage = require("../utils/authError.util");
 
 // Render the login page
-function getLogin (req, res) {
-    isAuthenticated(req)
-        ? res.redirect("/user-dashboard")
+async function getLogin (req, res) {
+    await isAuthenticated(req)
+        ? res.redirect("/dashboard")
         : res.render("pages/login", { title: "Login | URL Shortener" });
 }
 
 // Render the signup page
-function getSignup (req, res) {
-    isAuthenticated(req)
-        ? res.redirect("/user-dashboard")
+async function getSignup (req, res) {
+    await isAuthenticated(req)
+        ? res.redirect("/dashboard")
         : res.render("pages/signup", { title: "Sign Up | URL Shortener" });
 }
 
@@ -22,10 +23,18 @@ async function login (req, res) {
     if (!email || !password) return res.json({ success: false, message: "All fields are required" });
     try {
         const response = await signInWithEmail(email, password);
-        res.json({ success: true, message: "User logged in successfully", data: response });
+        const idToken = response._tokenResponse.idToken;
+        const refreshToken = response._tokenResponse.refreshToken;
+        await UserModel.updateOne(
+            { userId: response.user.uid },
+            { $set: { lastLogin: Date.now(), refreshToken: refreshToken } }
+        );
+        req.session.id_token = idToken;
+        res.cookie("id_token", idToken, { httpOnly: true });
+        res.status(200).json({ success: true, message: "User logged in successfully" });
     } catch (error) {
         console.log(error.message);
-        res.json({ success: false, message: error.message });
+        res.json({ success: false, message: getAuthErrorMessage(error) });
     }
 }
 
@@ -35,10 +44,22 @@ async function signUp(req, res) {
     if (!name || !email || !password) return res.json({ success: false, message: "All fields are required" });
     try {
         const response = await signUpWithEmail(email, password);
-        res.json({ success: true, message: "User created successfully", data: response });
+        const idToken = response._tokenResponse.idToken;
+        const refreshToken = response._tokenResponse.refreshToken;
+        const user = new UserModel({
+            name: name,
+            email: email,
+            userId: response.user.uid,
+            provider: "email",
+            refreshToken: refreshToken
+        });
+        await user.save();
+        req.session.id_token = idToken;
+        res.cookie("id_token", idToken, { httpOnly: true });
+        res.status(201).json({ success: true, message: "User created successfully" });
     } catch (error) {
         console.log(error.message);
-        res.json({ success: false, message: error });
+        res.json({ success: false, message: getAuthErrorMessage(error) });
     }
 }
 
