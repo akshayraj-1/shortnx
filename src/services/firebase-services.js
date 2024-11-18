0a3568e0aa9a1ebf340d5353d2066e70eb66e4bb
akshayraj-1 <akshayraj4101@gmail.com>
2024-11-19 01:04:11 +0530

    Added methods for the login/sign up with email functionality

--- src/services/firebase-services.js
+++ src/services/firebase-services.js
@@ -1,8 +1,9 @@
 /**
- * NOTE: The firebase sdk is meant to be used on the client side not the server side
- *       For server side you should use firebase-admin sdk only,
- *       but since I didn't want to share the firebase config data with the client side
- *       I am using both firebase (auth) and firebase-admin (generating access tokens) sdk on the server side to authenticate the user
+ * @note
+ * The firebase sdk is meant to be used on the client side not the server side
+ * For server side you should use firebase-admin sdk only,
+ * but since I didn't want to share the firebase config data with the client side
+ * I am using both firebase (auth) and firebase-admin (generating access tokens) sdk on the server side to authenticate the user
  */
 
 const app = require("firebase/app");
@@ -29,8 +30,33 @@ async function signUpWithEmail(email, password) {
     return response;
 }
 
+// Verify the user's id token
 async function verifyIdToken(idToken) {
-    return await admin.auth().verifyIdToken(idToken);
+    const response = await admin.auth().verifyIdToken(idToken);
+    return {
+        uid: response.uid,
+        email: response.email,
+        exp: response.exp * 1000
+    }
 }
 
-module.exports = { signInWithEmail, signUpWithEmail, verifyIdToken };
+// Regenerate the user's id token if the token is expired
+async function refreshIdToken(refreshToken) {
+    const url = `https://securetoken.googleapis.com/v1/token?key=${firebaseConfig.apiKey}`;
+    const response = await fetch(url, {
+        method: "POST",
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+        body: JSON.stringify({
+            grant_type: "refresh_token",
+            refresh_token: refreshToken
+        })
+    });
+    const data = await response.json();
+    return {
+        uid: data.user_id,
+        refresh_token: data.refresh_token,
+        exp: Date.now() + (data.expires_in * 1000)
+    }
+}
+
+module.exports = { signInWithEmail, signUpWithEmail, verifyIdToken, refreshIdToken };
