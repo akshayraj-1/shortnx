1ffbd8af9728cf5844bf24b75a0ba55ceddc9d94
akshayraj-1 <akshayraj4101@gmail.com>
2024-11-19 01:03:15 +0530

    Added functionality for validating the user

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,14 +1,35 @@
-const  { verifyIdToken } = require("../services/firebase-services");
+const UserModel = require("../models/user.model");
+const  { verifyIdToken, refreshIdToken } = require("../services/firebase-services");
+const logManager = require("../utils/logManager.util");
+
 async function isAuthenticated(req) {
     const idToken = req.cookies.id_token || req.session.id_token;
-    if (!idToken) return false;
+    const exp = req.session.id_token_exp || 0;
+    if (idToken && Date.now() < exp) {
+        logManager.logInfo("Token found in session for user:", req.session?.user?.uid);
+        return true;
+    }
     try {
-        const decodedToken = await verifyIdToken(idToken);
-        if (!decodedToken) return false;
-        req.session.user = decodedToken;
+        const data = await verifyIdToken(idToken);
+        req.session.id_token_exp = data.exp;
+        req.session.user = { uid: data.uid };
+        logManager.logInfo("Token verified for user", req.session.user.uid);
         return true;
     } catch (error) {
-        console.log(error);
+        logManager.logError(error);
+        // Refresh Token if the token is expired
+        if (error.code === "auth/id-token-expired") {
+            try {
+                const user = await UserModel.findOne({ userId: req.session.user.uid }, { refreshToken: 1 });
+                const data = await refreshIdToken(user.refreshToken);
+                await UserModel.updateOne({ userId: req.session?.user?.uid }, { $set: { refreshToken: data.refresh_token } });
+                req.session.id_token_exp = data.exp;
+                logManager.logInfo("Token Refreshed for user:", req.session?.user?.uid);
+                return true;
+            } catch (error) {
+                logManager.logError(error);
+            }
+        }
         return false;
     }
 }
