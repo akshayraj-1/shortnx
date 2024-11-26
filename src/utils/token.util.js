72a6a12260952b36b64bbf27e4069105d23ea9c1
akshayraj-1 <akshayraj4101@gmail.com>
2024-11-26 17:49:15 +0530

    Modify token.util.js

--- src/utils/token.util.js
+++ src/utils/token.util.js
@@ -17,26 +17,20 @@ function decodeToken(token) {
  * @throws {Promise<{code: "NO_USER_ID", message: string}>} - Rejects with an error code and message if there is no user ID.
  */
 function getRefreshToken(userId) {
-    if (!userId) return Promise.reject({
-        code: "NO_USER_ID",
-        message: "No user ID"
-    });
+    if (!userId) return Promise.reject({ code: "NO_USER_ID", message: "No user ID" });
     return Promise.resolve(generateToken({ userId }));
 }
 
 /**
  * Generates an access token using a refresh token.
+ * Since the refreshToken are not exposed to the client they can be just decoded to get the userId.
+ * No need to verify the signature of the refreshToken in this case.
  *
  * @param {string} refreshToken - The refresh token to decode for user information.
  * @returns {Promise<string>} - A promise that resolves to the generated access token.
- * @throws {Promise<{code: "INVALID_REFRESH_TOKEN", message: string}>} - Rejects with an error code and message if the refresh token is invalid.
  */
 function getAccessToken(refreshToken) {
     const { userId } = decodeToken(refreshToken);
-    if (!userId) return Promise.reject({
-        code: "INVALID_REFRESH_TOKEN",
-        message: "Invalid refresh token"
-    });
     return Promise.resolve(generateToken({ userId }, "1d"));
 }
 
@@ -45,28 +39,17 @@ function getAccessToken(refreshToken) {
  *
  * @param {string} accessToken - The access token to validate.
  * @returns {Promise<{userId: string}>} - A promise that resolves with the user ID if the token is valid.
- * @throws {Promise<{code: "NO_ACCESS_TOKEN" | "INVALID_ACCESS_TOKEN" | "ACCESS_TOKEN_EXPIRED", message: string}>} - Rejects with an error code and message if the token is invalid or expired.
+ * @throws {Promise<{code: "INVALID_ACCESS_TOKEN" | "ACCESS_TOKEN_EXPIRED", message: string}>} - Rejects with an error code and message if the token is invalid or expired.
  */
 function validateAccessToken(accessToken) {
-    if (!accessToken) return Promise.reject({
-        code: "NO_ACCESS_TOKEN",
-        message: "No access token"
-    });
     try {
-        const { userId } = jsonwebtoken.verify(accessToken, process.env.JWT_SECRET);
-        if (!userId) return Promise.reject({
-            code: "INVALID_ACCESS_TOKEN",
-            message: "Invalid access token"
-        });
+        const { userId, iss } = jsonwebtoken.verify(accessToken, process.env.JWT_SECRET);
+        if (!userId || iss !== "url-shortener") throw { code: "INVALID_ACCESS_TOKEN", message: "Invalid access token" };
         return Promise.resolve({ userId });
     } catch (error) {
-        if (error instanceof jsonwebtoken.TokenExpiredError) return Promise.reject({
-            code: "ACCESS_TOKEN_EXPIRED",
-            message: "Access token expired"
-        });
-        else return Promise.reject({
-            code: "INVALID_ACCESS_TOKEN",
-            message: "Invalid access token"
+        return Promise.reject({
+            code: error instanceof jsonwebtoken.TokenExpiredError ? "ACCESS_TOKEN_EXPIRED" : "INVALID_ACCESS_TOKEN",
+            message: error instanceof jsonwebtoken.TokenExpiredError ? "Access token expired" : "Invalid access token"
         });
     }
 }
