aae6ae5d9dbad04b143d3caf2964ef95ba426c62
akshayraj-1 <akshayraj4101@gmail.com>
2024-11-29 05:30:55 +0530

    Modify home.controller.js

--- src/controllers/home.controller.js
+++ src/controllers/home.controller.js
@@ -5,7 +5,7 @@ const { isAuthenticated } = require("../middlewares/auth.middleware");
 async function getHome(req, res) {
     await isAuthenticated(req, res)
         ? res.redirect("/dashboard")
-        : res.render("pages/home", { title: "Shortn - URL Shortener", navBg: "bg-colorBackground/85" });
+        : res.render("pages/home", { title: "Shortn - URL Shortener" });
 }
 
 module.exports = { getHome };
