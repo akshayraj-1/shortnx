10606237b2e7d8104b5ecbbc473cf5f44a77948b
akshayraj-1 <akshayraj4101@gmail.com>
2024-11-29 14:38:01 +0530

    Modify auth.middleware.js

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -3,27 +3,30 @@ const logManager = require("../utils/logManager.util");
 const UserModel = require("../models/user.model");
 
 async function isAuthenticated(req, res) {
-    const accessToken = req.cookies.access_token || req.session.access_token;
+    const accessToken = req.cookies.access_token || req.session?.access_token;
     try {
         const payload = await validateAccessToken(accessToken);
         // Assign the user details to the session
-        if (!req.session || !req.session.user) {
-            const user = await UserModel.findOne({ userId: payload.userId });
+        if (!req.session || !req.session.user || !res.locals.user) {
+            const user = await UserModel.findOne({ userId: payload.userId }, { _id: 0, userId: 1, name: 1, email: 1 });
             if (!user) return false;
-            req.session.user = { userId: user.userId, name: user.name, email: user.email };
+            res.locals.user = user;
+            req.session.user = user;
         }
         logManager.logInfo("Access token validated");
         return true;
     } catch (error) {
         if (error.code === "ACCESS_TOKEN_EXPIRED") {
             const payload = decodeToken(accessToken);
-            const user = await UserModel.findOne({ userId: payload.userId });
+            const user = await UserModel.findOne({ userId: payload.userId }, { _id: 0, userId: 1, name: 1, email: 1, refreshToken: 1 });
             if (!user) return false;
             const newAccessToken = await getAccessToken(user.refreshToken);
             // Assign the new access token and the user details to the session
             req.session.access_token = newAccessToken;
             res.cookie("access_token", newAccessToken, { httpOnly: true });
-            req.session.user = { userId: user.userId, name: user.name, email: user.email };
+            delete user.refreshToken;
+            res.locals.user = user;
+            req.session.user = user;
             logManager.logInfo("Access token refreshed");
             return true;
         }
