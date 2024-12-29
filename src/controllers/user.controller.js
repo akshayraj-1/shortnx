9fcfefd9d90075a472aa2d8d8746e65ac4857276
akshayraj-1 <akshayraj4101@gmail.com>
2024-12-29 22:14:36 +0530

    Update related to the user dashboard

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -2,6 +2,10 @@ const { validateAuthUser } = require("../middlewares/auth.middleware");
 
 
 const getDashboard = [validateAuthUser, (req, res, next) => {
+    const query = req.query;
+    if (!query.tab) {
+        return res.redirect("/u/dashboard/?tab=overview");
+    }
     res.render("layouts/dashboard-layout.ejs", { title: "Shortnx - Dashboard" });
 }];
 
@@ -9,29 +13,19 @@ const getTabContent = [validateAuthUser, async (req, res) => {
     const { tab } = req.params;
     switch (tab.toLowerCase()) {
         case "overview":
-            res.render("pages/dashboard/overview.ejs", {
-                user: req.user
-            });
+            res.render("pages/dashboard/overview.ejs");
             break;
         case "analytics":
-            res.render("pages/dashboard/analytics.ejs", {
-                user: req.user
-            });
+            res.render("pages/dashboard/analytics.ejs");
             break;
         case "links":
-            res.render("pages/dashboard/links.ejs", {
-                user: req.user
-            });
+            res.render("pages/dashboard/links.ejs");
             break;
         case "pages":
-            res.render("pages/dashboard/pages.ejs", {
-                user: req.user
-            });
+            res.render("pages/dashboard/pages.ejs");
             break;
         case "settings":
-            res.render("pages/dashboard/settings.ejs", {
-                user: req.user
-            });
+            res.render("pages/dashboard/settings.ejs");
             break;
     }
 }];
